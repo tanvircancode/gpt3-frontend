92845ac6c242f5090655ab0e375b0e3db6d7f495
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2024-05-02 15:02:47 +0600

    Updated upto whatgpt3

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,26 @@
 import "./brand.css";
+import { google, slack, dropbox, atlassian, shopify } from "./imports";
 
 const Brand = () => {
   return (
-    <div>Brand</div>
-  )
-}
+    <div className="gpt3__brand section__padding">
+      <div> 
+        <img src={google} />
+      </div>
+      <div>
+        <img src={dropbox} />
+      </div>
+      <div>
+        <img src={slack} />
+      </div>
+      <div>
+        <img src={atlassian} />
+      </div>
+      <div>
+        <img src={shopify} />
+      </div>
+    </div>
+  );
+};
 
-export default Brand
+export default Brand;
