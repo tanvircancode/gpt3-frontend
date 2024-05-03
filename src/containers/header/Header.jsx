c296e1c4f7e70ce7701e39ca6259e7ca9db63570
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2024-05-03 22:02:32 +0600

    updated upto possibilities section

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -4,7 +4,7 @@ import ai from "../../assets/ai_header.png";
 
 const Header = () => {
   return (
-    <div className="gpt3__header section__padding" id="home">
+    <div className="gpt3__header" id="home">
       <div className="gpt3__header-content">
         <h1 className="gradient__text">
           Let’s Build Something amazing with GPT-3 OpenAI
