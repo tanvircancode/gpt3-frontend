a8abbaa97d51670939be7e98e1d5abcc67feda7e
Tanvir Ahmed Chowdhury <tanahmchw241996@gmail.com>
2024-05-06 23:59:06 +0600

    finishing footer

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,10 +1,23 @@
-
-
 import "./footer.css";
+import logo from "../../assets/GPT-3.svg"
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="gpt3__footer section__padding">
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">
+          Do you want to step in to the future before others
+        </h1>
+      </div>
+      <div className="gpt3__footer-btn">
+        <p>Request Early Access</p>
+      </div>
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={logo} alt="gpt3_logo" /> 
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
